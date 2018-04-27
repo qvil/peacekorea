78f35ddba25398728df8a23a4ae029b00576ed7e
qvil <qvil1127@gmail.com>
2018-04-27 18:59:45 +0900

    First prototype

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 2018-04-27 금요일 한국시간 18 시  
 대한민국은 종전선언을 하고 평화국가가 되었습니다.
 
-Fri, 27 April 2018, 18:00 (Korea Time, UCT+09:00 seoul)
+Fri, 27 April 2018, 18:00 (Korea Time, UCT+09:00 seoul)  
 The Republic of Korea declared the end of war and became a peacuful nation.
 
 # [Commemorative page](https://qvil.github.io/peacekorea/)
+
+https://qvil.github.io/peacekorea/
 
 # [License](https://github.com/qvil/peacekorea/blob/master/LICENSE)
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
+import Image1 from "./20180427165016974keev.jpg";
 
 class App extends Component {
+  styles = {
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%"
+    },
+    img: {
+      position: "fixed",
+      opacity: 0.2
+    },
+    text: {
+      zIndex: 2
+    }
+  };
   render() {
-    return <div className="App">App</div>;
+    const { styles } = this;
+
+    return (
+      <div style={styles.root}>
+        <img style={styles.img} src={Image1} />
+        <h2 style={styles.text}>
+          2018-04-27 금요일 한국시간 18 시 <br />
+          대한민국은 종전선언을 하고 평화국가가 되었습니다. <br />
+          <br />
+          Fri, 27 April 2018, 18:00 (Korea Time, UCT+09:00 seoul) <br />
+          The Republic of Korea declared the end of war and became a peacuful
+          nation.
+        </h2>
+        {/* <div>123123</div> */}
+      </div>
+    );
   }
 }
 
